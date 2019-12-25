8ce5ed81055f2d95ef48d0bbef5b646436e30d39
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2019-12-25 14:29:28 +0700

    add CORS configuration

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
