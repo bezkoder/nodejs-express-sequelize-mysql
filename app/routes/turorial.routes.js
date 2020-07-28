37fb9a0d0e759191d693ce3b508bab0db80455bc
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-07-28 07:56:56 +0700

    fix instruction comment

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -21,7 +21,7 @@ module.exports = app => {
   // Delete a Tutorial with id
   router.delete("/:id", tutorials.delete);
 
-  // Create a new Tutorial
+  // Delete all Tutorials
   router.delete("/", tutorials.deleteAll);
 
   app.use('/api/tutorials', router);
